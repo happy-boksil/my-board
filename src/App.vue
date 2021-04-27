319724718e78f7df0847e2b18463c3d1ec7d790a
happy-boksil2 <happyBoksil2@gmail.com>
2021-04-27 23:36:51 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <my-board-header></my-board-header>
+    <my-board-body></my-board-body>
+  </div>
+</template>
+
+<script>
+import MyBoardHeader from './components/MyBoardHeader.vue'
+import MyBoardBody from './components/MyBoardBody.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyBoardHeader,
+    MyBoardBody
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyBoardBody.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="body">body</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#body {
+    background-color: #2c3e50;
+}
+</style>
